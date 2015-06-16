a2b4e2c39aba24e533906a0cc50fef6e6b77a92b
Xavier Shay <xavier@rhnh.net>
2015-06-15 19:24:25 -0700

    Push state, permalinks, and page titles.

--- lib/server.rb
+++ lib/server.rb
@@ -5,14 +5,17 @@ require 'pacebot'
 bot = Pacebot.new
 
 post '/' do
+  input = params['text']
   text = if params['user_name'] != 'slackbot'
-    input = params['text']
     resp = bot.parse(input)
 
     resp && bot.format(resp)
   end
 
-  {text: text}.to_json
+  {
+    text: text,
+    input: input
+  }.to_json
 end
 
 get '/' do
