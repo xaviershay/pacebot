b7cb8fc54b9425a47ea5fe9dec17b1708cf54518
Xavier Shay <contact@xaviershay.com>
2018-08-16 18:23:55 -0700

    Add option for help response when no parse.

--- lib/server.rb
+++ lib/server.rb
@@ -9,7 +9,11 @@ post '/' do
   text = if params['user_name'] != 'slackbot'
     resp = bot.parse(input)
 
-    resp && bot.format(resp)
+    if resp
+      bot.format(resp)
+    elsif params[:help_on_unknown]
+      "examples: 5 mi, 4:00 mi, 3:10 kms, 3 mi @ 4:20, 3 mi in 12:50"
+    end
   end
 
   {
